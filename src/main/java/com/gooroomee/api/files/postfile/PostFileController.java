18d5ab959188415cafef76cb1b85e33523dad5fc
jinyoung.kim <jinyoung.kim@gooroomee.com>
2020-03-18 16:42:10 +0900

    GT-12 inputStream,outputStream 으로 로직 변경

--- src/main/java/com/gooroomee/api/files/postfile/PostFileController.java
+++ src/main/java/com/gooroomee/api/files/postfile/PostFileController.java
@@ -2,6 +2,7 @@ package com.gooroomee.api.files.postfile;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -11,6 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,31 +24,25 @@ public class PostFileController {
 
     private final PostFileService postFileService;
 
-    @PostMapping("/uploadFile")
-    public UploadFileResponse uploadFile(@RequestParam("file") MultipartFile file){
-        PostFile postFile = postFileService.storeFile(file);
-        String fileDownloadUri= ServletUriComponentsBuilder.fromCurrentContextPath()
-                .path("/downloadFile/")
-                .path(postFile.getFile_id())
-                .toUriString();
-        return new UploadFileResponse(postFile.getFile_name(), fileDownloadUri, file.getContentType(), file.getSize());
+    @PostMapping("/testUpload/{post_id}")
+    public UploadFileResponse upload(@RequestParam("file") MultipartFile file,
+                                     @PathVariable Long post_id) {
+        PostFile postFile = this.postFileService.storeFile(file, post_id);
+        return new UploadFileResponse(postFile.getFile_name(), "/testDownload/"+postFile.getFile_id(), file.getContentType(), file.getSize());
     }
 
-    @PostMapping("/uploadMultipleFiles")
-    public List<UploadFileResponse> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files){
-        return Arrays.asList(files)
-                .stream()
-                .map(file -> uploadFile(file))
-                .collect(Collectors.toList());
-    }
 
-    @GetMapping("/downloadFile/{fileId}")
-    public ResponseEntity<Resource> downloadFile(@PathVariable String fileId, HttpServletResponse response){
-        PostFile postFile = postFileService.getFile(fileId);
-        return ResponseEntity.ok().contentType(MediaType.parseMediaType(postFile.getFile_type()))
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment: filename=\""+ postFile.getFile_name()+"\"")
-                .body(new ByteArrayResource(postFile.getFile_data()));
-
+    @GetMapping("/testDownload/{file_id}")
+    public ResponseEntity<InputStreamResource> testDownload(@PathVariable String file_id) throws IOException{
+        PostFile postFile = this.postFileService.getFile(file_id);
+        String Path = postFile.getFile_url();
+        File file = new File(Path);
+        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION,
+                        "attachment;filename="+file.getName())
+                .contentType(MediaType.parseMediaType(postFile.getFile_type())).contentLength(file.length())
+                .body(resource);
     }
 
 }
--- src/main/java/com/gooroomee/api/files/postfile/PostFileService.java
+++ src/main/java/com/gooroomee/api/files/postfile/PostFileService.java
@@ -9,7 +9,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 import javax.transaction.Transactional;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Service
 @RequiredArgsConstructor
@@ -20,19 +23,35 @@ public class PostFileService {
     private PostFileRepository postFileRepository;
 
     @Transactional
-    public PostFile storeFile(MultipartFile file) {
+    public PostFile storeFile(MultipartFile file, Long post_id) {
+        String test_file_path="C:\\Users\\gooroomee\\uploads\\"+post_id+"\\";
+        File f=new File(test_file_path);
+        if(!f.exists()) f.mkdirs();
+
         String fileName = StringUtils.cleanPath(file.getOriginalFilename());
-
-        try {
-            if(fileName.contains("..")) {
-                throw new FileUploadException("파일 이름에 이상값이 들어있습니다. " + fileName);
-            }
-            PostFile postFile =new PostFile(fileName,file.getContentType(), file.getBytes());
-            return postFileRepository.save(postFile);
-
-        } catch (IOException ex) {
-            throw new FileUploadException(fileName + " 파일을 저장할 수 없습니다. 다시 시도해주세요!", ex);
+        if(fileName.contains("..")) {
+            throw new FileUploadException("파일 이름에 이상값이 들어있습니다. " + fileName);
         }
+        PostFile postFile = PostFile.builder()
+                .file_name(file.getOriginalFilename())
+                .file_type(file.getContentType())
+                .file_url(test_file_path+file.getOriginalFilename())
+                .build();
+        try (
+                FileOutputStream fos = new FileOutputStream(test_file_path + fileName);
+                InputStream is = file.getInputStream();) {
+
+            int readCount = 0;
+            byte[] buffer = new byte[1024];
+
+            while ((readCount = is.read(buffer)) != -1) {
+                fos.write(buffer, 0, readCount);
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException("file Save Error");
+        }
+
+        return postFileRepository.save(postFile);
 
     }
 
